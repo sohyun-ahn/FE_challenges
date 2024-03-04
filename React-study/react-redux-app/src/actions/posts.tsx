102b1216689744f28b7b35761143439b8a98e18d
Sohyun Ahn <71028309+sohyun-ahn@users.noreply.github.com>
2024-03-04 17:14:17 +0900

    Delete React-study/react-redux-app directory

--- React-study/react-redux-app/src/actions/posts.tsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const getchPosts = (): any => async (dispatch: any, getState: any) => {
-  const response = await axios.get(
-    "https://jsonplaceholder.typicode.com/posts"
-  );
-  dispatch({ type: "FETCH_POSTS", payload: response.data });
-};
