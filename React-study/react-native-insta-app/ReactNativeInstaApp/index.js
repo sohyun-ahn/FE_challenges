8c14f1140bdc8ba324028b9c79cb511f08056207
sohyun-ahn <sohyun5747@gmail.com>
2024-03-22 02:08:11 +0900

    feat: Implement routing using bottom tab navigator, native stack navigator

--- React-study/react-native-insta-app/ReactNativeInstaApp/index.js
+++ React-study/react-native-insta-app/ReactNativeInstaApp/index.js
@@ -5,5 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
+// AppRegistry는 모든 React Native App을 실행하기 위한 JS진입점
+// App 루트 컴포넌트는 자신을 등록해야함.
 AppRegistry.registerComponent(appName, () => App);
